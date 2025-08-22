f398c34651cfb7ea62f4ab5dcf7fb8261ffbf146
Vignesh V Pai <vigneshpai444@gmail.com>
2025-08-22 16:29:29 +0200

    Created SSDDPM basic model

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import lightning as L
+from lightning.pytorch.loggers import TensorBoardLogger
 from src.model.SSDDPM import SSDDPM
 from src.data.DWIDataLoader import DWIDataLoader
 from src.config.config import Config
@@ -13,8 +14,16 @@ def main():
         out_channels=Config.SSDDPM_CONFIG["out_channels"],
     )
 
-    # Set up the trainer using max_epochs from config
-    trainer = L.Trainer(max_epochs=Config.MAX_EPOCHS)
+    # Set up the TensorBoard logger
+    logger = TensorBoardLogger("tb_logs", name="ssddpm")
+
+    # Set up the trainer using max_epochs from config and the logger
+    trainer = L.Trainer(
+        max_epochs=Config.MAX_EPOCHS,
+        logger=logger,
+        log_every_n_steps=1,
+        enable_checkpointing=False,
+    )
 
     # Train the model
     trainer.fit(model, datamodule=data_module)
