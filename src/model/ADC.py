bc40c171e99b77282e0d7b60bb25c66b63ff9ba0
Vignesh V Pai <vigneshpai444@gmail.com>
2025-08-28 12:09:50 +0200

    Created training step algorithm

---
+++ src/model/ADC.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class ADC(nn.Module):
+    def __init__(self, b_values):
+        super().__init__()
+
+    def forward(self, y_prime_t_minus_1):
+       
--- src/model/SSDDPM.py
+++ src/model/SSDDPM.py
@@ -22,6 +22,7 @@ class SSDDPM(L.LightningModule):
         )
 
         self.scheduler = DDPMScheduler(**Config.SCHEDULER_CONFIG)
+        self.lambda_reg = Config.LAMBDA_REG
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), **Config.OPTIMIZER_CONFIG)
@@ -56,6 +57,18 @@ class SSDDPM(L.LightningModule):
             self.scheduler.betas[steps]
         ) * epsilon_zero  # Step 7: y'_{t-1} = (1 / √1 - β_t) (y_t - (β_t / √1 - α_t) ê_t) + √β_t ε₀
 
-        loss = torch.nn.functional.mse_loss(residual, noise)
-        self.log("train_loss", loss, prog_bar=True)
+        S0_hat, D_hat = self.f_ADC(y_prime_t_minus_1)  # Step 8: Ŝ₀, D̂ ← f_ADC(y'_{t-1})
+
+        y_hat_t_minus_1 = S0_hat * torch.exp(
+            -self.b * D_hat
+        )  # Step 9: ŷ_{t-1} ← Ŝ₀ e^(-b D̂)
+
+        noise_loss = torch.nn.functional.mse_loss(residual, noise)  # ||ê_t - ε||²₂
+        reg_loss = torch.nn.functional.mse_loss(
+            y_hat_t_minus_1, y_prime_t_minus_1
+        )  # ||ŷ_{t-1} - y'_{t-1}||²₂
+        loss = (
+            noise_loss + self.lambda_reg * reg_loss
+        )  # Step 10: ||ê_t - ε||²₂ + λ ||ŷ_{t-1} - y'_{t-1}||²₂
+
         return loss
