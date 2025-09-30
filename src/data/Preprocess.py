Fixed padding and 0-1 normalisation

src/data/Preprocess.py
@@ -5,7 +5,7 @@ from src.config.config import Config
 
 class Preprocess:
     @staticmethod
-    def normalize_to_b0(image):
+    def normalize_to_0_1(image):
         """
         Normalize the image to the 0-1 range globally.
         Args:
@@ -28,7 +28,7 @@ class Preprocess:
             raise ValueError(
                 f"Expected image of shape (width, height, bvalues), got {image.shape}"
             )
-        # Permute to (bvalues, height, width)
+        # Permute to (bvalues, width, height)
         image = image.permute(2, 0, 1)
 
         return image
@@ -36,30 +36,34 @@ class Preprocess:
     @staticmethod
     def pad_to_unet_compatible(image, target_shape=None):
         """
-        Pad the image tensor with zeros to make width and height match target_shape.
+        Pad the image tensor with zeros to make height and width match target_shape.
         Args:
-            image (torch.Tensor): Image tensor of shape (width, height, bvals).
-            target_shape (tuple): (target_height, target_width)
+            image (torch.Tensor): Image tensor of shape (width, height, bvalues).
+            target_shape (tuple): (target_width, target_height)
         Returns:
             torch.Tensor: Zero-padded image tensor.
         """
         if target_shape is None:
             target_shape = Config.UNET_COMPATIBLE_SHAPE
 
-        # image shape: (width, height, slices, bvals)
+        # image shape: (width, height, bvalues)
         w, h, b = image.shape
-        target_h, target_w = target_shape
+        target_w, target_h = target_shape
 
         pad_h = max(target_h - h, 0)
         pad_w = max(target_w - w, 0)
 
-        # Calculate padding for width and height dimensions
-        pad_left_w = pad_w // 2
-        pad_right_w = pad_w - pad_left_w
+        # Calculate padding for height and width dimensions
         pad_left_h = pad_h // 2
         pad_right_h = pad_h - pad_left_h
+        pad_left_w = pad_w // 2
+        pad_right_w = pad_w - pad_left_w
 
-        # For padding width and height (first two dimensions)
+        # For torch.nn.functional.pad, dimensions are padded from right to left
+        # For shape (width, height, bvalues):
+        # - Dimension 2 (bvalues): no padding
+        # - Dimension 1 (height): pad_left_h, pad_right_h
+        # - Dimension 0 (width): pad_left_w, pad_right_w
         pad = (0, 0, pad_left_h, pad_right_h, pad_left_w, pad_right_w)
         # Note: torch.nn.functional.pad() always creates new memory
         image_padded = torch.nn.functional.pad(image, pad)
@@ -67,8 +71,8 @@ class Preprocess:
 
     @staticmethod
     def preprocess(image):
+        image, min_val, max_val = Preprocess.normalize_to_0_1(image)
         image = Preprocess.pad_to_unet_compatible(image)
-        image, min_val, max_val = Preprocess.normalize_to_b0(image)
         image = Preprocess.reorder_bvals(image)
 
         return image, min_val, max_val

src/model/SSDDPM.py
@@ -88,8 +88,6 @@ class SSDDPM(L.LightningModule):
                 noisy_images - (betas / torch.sqrt(1 - alphas_cumprod)) * residual
             )
 
-        y_prime_t_minus_1, _, _ = Preprocess.normalize_to_b0(y_prime_t_minus_1)
-
         return y_prime_t_minus_1
 
     def _get_y_hat_t_minus_1(self, S0_hat, D_hat, b_values):
@@ -111,8 +109,6 @@ class SSDDPM(L.LightningModule):
         y_hat_t_minus_1 = S0_hat_expanded * torch.exp(
             -b_values_reshaped * D_hat_expanded
         )
-
-        y_hat_t_minus_1, _, _ = Preprocess.normalize_to_b0(y_hat_t_minus_1)
 
         return y_hat_t_minus_1
 
@@ -220,8 +216,10 @@ class SSDDPM(L.LightningModule):
 
         y_hat_t_minus_1 = self._get_y_hat_t_minus_1(S0_hat, D_hat, b_values)
 
+        eps = 1e-8
+
         adc_loss = torch.nn.functional.mse_loss(
-            y_hat_t_minus_1, y_prime_t_minus_1
+            torch.log(y_hat_t_minus_1 + eps), torch.log(y_prime_t_minus_1 + eps)
         )  # Self-supervised: ||ŷ_{t-1} - f₀(ŷ_{t-1}, t)||²₂
         self.log(
             f"{mode}_adc_loss",
@@ -243,52 +241,52 @@ class SSDDPM(L.LightningModule):
             batch_size=Config.BATCH_SIZE,
         )
 
-        if mode == "val" and (
-            self.current_epoch % Config.CHECKPOINT_CONFIG["every_n_epochs"] == 0
-        ):
-            self._log_specific_slice(
-                images,
-                b_values,
-                other_info,
-                step_or_epoch=self.current_epoch,
-                prefix=mode,
-                save_dir=f"{mode}_images/{self.run_name}/original_images",
-            )
-            # self._log_specific_slice(
-            #     noisy_images,
-            #     b_values,
-            #     other_info,
-            #     step_or_epoch=self.current_epoch,
-            #     prefix=mode,
-            #     save_dir=f"{mode}_images/{self.run_name}/noisy_images",
-            # )
-            self._log_specific_slice(
-                residual,
-                b_values,
-                other_info,
-                step_or_epoch=self.current_epoch,
-                prefix=mode,
-                save_dir=f"{mode}_images/{self.run_name}/residual_images",
-            )
-            # self._log_specific_slice(
-            #     y_hat_t_minus_1,
-            #     b_values,
-            #     other_info,
-            #     step_or_epoch=self.current_epoch,
-            #     prefix=mode,
-            #     save_dir=f"{mode}_images/{self.run_name}/y_hat_t_minus_1",
-            # )
-
-            denoised_images = self.inference(noisy_images, b_values)
-            self._log_specific_slice(
-                denoised_images,
-                b_values,
-                other_info,
-                step_or_epoch=self.current_epoch,
-                prefix=mode,
-                save_dir=f"{mode}_images/{self.run_name}/denoised_images",
-            )
-            del denoised_images
+        # if mode == "val" and (
+        #     self.current_epoch % Config.CHECKPOINT_CONFIG["every_n_epochs"] == 0
+        # ):
+        # self._log_specific_slice(
+        #     images,
+        #     b_values,
+        #     other_info,
+        #     step_or_epoch=self.current_epoch,
+        #     prefix=mode,
+        #     save_dir=f"{mode}_images/{self.run_name}/original_images",
+        # )
+        # self._log_specific_slice(
+        #     noisy_images,
+        #     b_values,
+        #     other_info,
+        #     step_or_epoch=self.current_epoch,
+        #     prefix=mode,
+        #     save_dir=f"{mode}_images/{self.run_name}/noisy_images",
+        # )
+        # self._log_specific_slice(
+        #     residual,
+        #     b_values,
+        #     other_info,
+        #     step_or_epoch=self.current_epoch,
+        #     prefix=mode,
+        #     save_dir=f"{mode}_images/{self.run_name}/residual_images",
+        # )
+        # self._log_specific_slice(
+        #     y_hat_t_minus_1,
+        #     b_values,
+        #     other_info,
+        #     step_or_epoch=self.current_epoch,
+        #     prefix=mode,
+        #     save_dir=f"{mode}_images/{self.run_name}/y_hat_t_minus_1",
+        # )
+        # if self.current_epoch == self.max_epochs - 1:
+        #     denoised_images = self.inference(noisy_images, b_values)
+        #     self._log_specific_slice(
+        #         denoised_images,
+        #         b_values,
+        #         other_info,
+        #         step_or_epoch=self.current_epoch,
+        #         prefix=mode,
+        #         save_dir=f"{mode}_images/{self.run_name}/denoised_images",
+        #     )
+        #     del denoised_images
 
         del (
             noise,